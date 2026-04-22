8c08809266e0111b06d8989f0e92057f67383055
Kelissa <kelissa_wallace@horacemann.org>
2026-04-22 09:06:32 -0400

    added readme

--- src/Game/ErrorCheck.java
+++ src/Game/ErrorCheck.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class ErrorCheck {
 
     public static int getInt(Scanner sc) {
-        return sc.nextInt(); // add error checking here!
+        return sc.nextInt();
+
     }
 }
