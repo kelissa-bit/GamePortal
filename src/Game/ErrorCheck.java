74b639d27d135811ddafb821f386baf2e9c0f334
Kelissa <kelissa_wallace@horacemann.org>
2026-04-24 11:04:43 -0400

    added error checking

--- src/Game/ErrorCheck.java
+++ src/Game/ErrorCheck.java
@@ -5,7 +5,12 @@ import java.util.Scanner;
 public class ErrorCheck {
 
     public static int getInt(Scanner sc) {
-        return sc.nextInt();
-
+        if (sc.hasNextInt()){
+            return sc.nextInt();
+        } else {
+            sc.nextLine();
+            System.out.println("Please type in a number from 1-3!");
+            return sc.nextInt();
+        }
     }
 }
